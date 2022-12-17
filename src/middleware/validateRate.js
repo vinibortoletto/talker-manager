f2830ef91207c1236a12c4a5bfe2777d0b4528e0
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2022-12-17 15:06:34 -0300

    refactor: rate conditional

--- src/middleware/validateRate.js
+++ src/middleware/validateRate.js
@@ -4,12 +4,13 @@ const validateRate = (req, res, next) => {
   const { rate } = req.body.talk;
   const rateRequired = 'O campo "rate" é obrigatório';
   const rateMustBeInteger = 'O campo "rate" deve ser um inteiro de 1 à 5';
+  const rateList = [1, 2, 3, 4, 5];
 
   if (rate === undefined) {
     return res.status(BAD_REQUEST).json({ message: rateRequired });
   }
   
-  if (!Number.isInteger(rate) || rate < 1 || rate > 5) {
+  if (!rateList.includes(rate)) {
     return res.status(BAD_REQUEST).json({ message: rateMustBeInteger });
   }
 
